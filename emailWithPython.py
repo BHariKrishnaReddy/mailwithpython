353c5155decbd821962ba3bab2523e0ea63bcc57
íต ॐ ᐯᏆしᗩᑎ <45511185+BHariKrishnaReddy@users.noreply.github.com>
2020-12-20 10:11:55 +0530

    Create emailWithPython.py

---
+++ emailWithPython.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = '--YOUR NAME--'
+email['to'] = 'Enter the mailID of your recepint'
+email['subject'] = 'Here you can enter the subject line of your mail '
+
+email.set_content('Your Email body goes here ..............')
+
+with smtplib.SMTP(host='smtp.gmail.com', port = 587 ) as smtp:
+  smtp.ehlo()0000000000
+  smtp.starttls()
+  smtp.login('SendermailID@gmail.com','Yoursecretpassword')
+  smtp.send(email)
+  
